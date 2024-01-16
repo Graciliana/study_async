605c2ef9cf81b6996492d2abc2ef580ebb867f9c
Graciliana Kascher <eagraciliana@gmail.com>
2024-01-16 17:08:13 -0300

    :sparkles: feat: Validation of registration fields

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,30 @@
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 
 def cadastro(request):
-  return render(request, 'cadastro.html')
+  if request.method == 'GET':
+    return render(request, 'cadastro.html')
+  else:
+    username = request.POST.get('username')
+    senha = request.POST.get('senha')
+    confirmar_senha = request.POST.get('confirmar_senha')
+    
+    if not senha == confirmar_senha:
+      messages.add_message(request, constants.ERROR, 'Senhas e confirmar senha  não coincídem.')
+      return redirect('/usuarios/cadastro')
+    
+    user = User.objects.filter(username=username)
+    if user.exists():
+      messages.add_message(request, constants.ERROR, 'Usuário já existe.')
+      return redirect('/usuarios/cadastro')
+    try:
+      user = User.objects.create_user( username=username,password=confirmar_senha)
+      return redirect('/usuarios/login')
+    except:
+      messages.add_message(request, constants.ERROR, 'Erro interno do servidor.')
+      return redirect('/usuarios/cadastro')
+    
